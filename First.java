3ef412d45c4ded9ce6b29b7144955fbffc8f7920
Arif ansari <124675486+Arifansari8299@users.noreply.github.com>
2024-09-12 10:46:34 +0530

    Add files via upload

---
+++ First.java
@@ -0,0 +1,18 @@
+/*
+ * java is a high level programming language that was developed by james gosling in 1995 in sun microsystem
+ * features:plateform independent, object oriented,plateform independt,simple ,secure,multithreaded,Robust,portable,high performance,dynamic,distributed
+ * it is used for desktop application and mobile application,web application
+ * 
+ * 
+ */
+
+public class First{
+      public static void main(String[] args) {
+            System.out.println("hello world");
+      }
+}
+
+// public : is a access specifier that is used to provide the accessibility 
+// class : is a keyword that is used to naming the className
+// First: is a className
+// public static void main(String[] args) : it is the main method or entry point
